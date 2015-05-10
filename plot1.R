3a20e6994800e42175a9594f35534c54c1bf974d
Andrew W. Hill <andrew.hill@daimler.com>
2015-05-10 11:50:29 -0700

    plot 1 complete

---
+++ plot1.R
@@ -0,0 +1,23 @@
+loadData<-function() {
+  table<-read.table("household_power_consumption.txt",header=TRUE,sep=";",colClasses=c("Date","factor","numeric","numeric","numeric","numeric","numeric","numeric","numeric"),na.strings="?")
+}
+
+makePlot<-function(reloadData=FALSE) {
+  if(reloadData) {
+    loadData()
+  }
+  
+  #Fix date format
+  table$Date<-as.Date(table$Date,"%d/%m/%Y")
+  
+  # Filter to February 1-2, 2007
+  filtered<-table
+  filtered<-filtered[filtered$Date>as.Date("31/1/2007","%d/%m/%Y"),]
+  filtered<-filtered[filtered$Date<as.Date("3/2/2007","%d/%m/%Y"),]
+  
+  active_power <- filtered$Global_active_power
+  hist(active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
+  
+  dev.copy(png, file = "plot1.png")
+  dev.off()
+}
